f5efb0a7aca4757538aebc04bac7298d08a9227a
Rishika Sharma <77633146+rishika1302@users.noreply.github.com>
2022-01-26 19:43:46 +0530

    ceil and floor

---
+++ Generic Tree/ceilFloor.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class ceilFloor {
+    public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter data to find its ceil and floor = ");
+        int data = scn.nextInt();
+        Node root = construct(arr);
+        ceil = Integer.MAX_VALUE;
+        floor = Integer.MIN_VALUE; 
+        ceilFloor(root, data);
+        System.out.println("Ceil - " + ceil);
+        System.out.println("Floor - " + floor);
+    }
+
+    static int ceil;
+    static int floor;
+    public static void ceilFloor(Node node, int data){
+        if(node.data > data){
+            if(node.data < ceil){
+                ceil = node.data;
+            }
+        }
+        if(node.data < data){
+            if(node.data > floor){
+                floor = node.data;
+            }
+        }
+        for(Node child : node.children){
+            ceilFloor(child, data);
+        }
+    }
+
+    public static Node construct(int[] arr){
+        Stack<Node> ms = new Stack<>();
+        Node root = null;
+        for(int val : arr){
+            if(val!=-1){
+                Node node = new Node(val);
+                ms.push(node);
+            }else{
+                Node node = ms.pop();
+                if(ms.size()>0){     
+                    Node parent = ms.peek();
+                    parent.children.add(node);
+                }else{
+                    root = node;
+                }
+            }
+        }
+        return root;
+    }
+}
