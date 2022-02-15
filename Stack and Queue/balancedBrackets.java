2a946cdf4c1722f270f8e977cbf3376a0583d55a
Rishika Sharma <77633146+rishika1302@users.noreply.github.com>
2022-02-15 23:53:19 +0530

    commit

--- Stack and Queue/balancedBrackets.java
+++ Stack and Queue/balancedBrackets.java
@@ -1,4 +1,51 @@
 import java.util.*;
+
+import javax.swing.text.DefaultStyledDocument.ElementSpec;
 public class balancedBrackets {
-    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        Stack<Character> ms = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            Character ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                ms.push(ch);
+            }else if(ch == ')'){
+                if(ms.size()==0){
+                    System.out.println(false);
+                    return;
+                }else if(ms.peek()!='('){
+                    System.out.println(false);
+                    return;
+                }else{
+                    ms.pop();
+                }
+            }else if(ch == '}'){
+                if(ms.size()==0){
+                    System.out.println(false);
+                    return;
+                }else if(ms.peek()!='{'){
+                    System.out.println(false);
+                    return;
+                }else{
+                    ms.pop();
+                }
+            }else if(ch == ']'){
+                if(ms.size()==0){
+                    System.out.println(false);
+                    return;
+                }else if(ms.peek()!='['){
+                    System.out.println(false);
+                    return;
+                }else{
+                    ms.pop();
+                }
+            }
+        }
+        if(ms.size()>0){
+            System.out.println(false);
+        }else{
+            System.out.println(true);
+        }
+    }
 }
